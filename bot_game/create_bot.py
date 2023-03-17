fa315070be9bcefeaf08c19d47c673f94476c01c
evgeny <8513@mail.ru>
2023-03-17 17:57:15 +0300

    bot_games

---
+++ bot_game/create_bot.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+bot = Bot("5601332796:AAEHRLMkigWsMK3CRgIabc6s5SMn-7tWuSY")
+dp =Dispatcher(bot)
